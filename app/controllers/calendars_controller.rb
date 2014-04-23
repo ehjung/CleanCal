44ef5c79c93d392d17ed2daaeb85af6744adf8f5
ehjung <ejung@thoughtworks.com>
2014-04-22 21:10:30 -0400

    Fix for calendar suddenly not showing

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -5,4 +5,19 @@ class CalendarsController < ApplicationController
 		@events = Event.where(scheduleid: params[:scheduleid]).all
 	end
 	
+	def create
+		@calendar = Calendar.new(calendar_params)
+		respond_to do |format|
+			if @calendar.save
+				format.html { redirect_to calendars_path }
+			else
+				format.html { render action: 'new' }
+	        	format.json { render json: @calendar.errors, status: :unprocessable_entity }
+	        end
+	    end
+	end
+
+	def calendar_params
+		params.require(:calendar)#.permit(:title, :start, :end, :scheduleid)
+	end
 end
